f066f7e209cb54699bfae3015ddcf6397d66a6b3
pirovalen <valen.araya@gmail.com>
2024-12-03 19:19:09 -0300

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react"; 
+import { GlobalStates } from "./utils/global.context"; 
+
 const Footer = () => {
+  const { state } = useContext(GlobalStates); 
   return (
-    <footer>
+    <footer className={state.theme}>
         <p>Powered by</p>
         <img src="./images/DH.png" alt='DH-logo' />
     </footer>
